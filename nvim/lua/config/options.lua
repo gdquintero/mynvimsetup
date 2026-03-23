b3b0499a5ba8211c754a693690b49e9c1123e426
Gustavo <gustavoquintero9105@gmail.com>
2026-03-23 16:50:18 -0500

    Updated on 03-23-2026 at 16:50

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.cmd("highlight CursorLine cterm=NONE gui=NONE guibg=Grey20")
