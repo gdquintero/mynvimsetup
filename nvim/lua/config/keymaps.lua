1bcbf2b6f21a6a81c144c3224dc1de2a8f3e03fd
Gustavo <gustavoquintero9105@gmail.com>
2026-03-23 20:49:44 -0500

    Updated on 03-23-2026 at 20:49

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "gb", ":bn<CR>", {noremap = true, silent = true})
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,3 +9,4 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
 vim.cmd("highlight CursorLine cterm=NONE gui=NONE guibg=Grey20")
+vim.opt.termguicolors = true
